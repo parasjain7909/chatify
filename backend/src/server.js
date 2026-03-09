003b94870e22574b42bd4757a9185521cca7f656
paras jain <parasjainooo123@gmail.com>
2026-03-09 10:36:31 +0530

    changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,10 +8,7 @@ const app= express();
 const port= process.env.PORT||5000;
 const __dirname=path.resolve();
 
-app.use(cors({
-    origin:"https://chatify-wine-eight.vercel.app",
-    credentials:true
-}))
+app.use(cors())
 
 
 
