dc96e556935e7078d34b086edd62f37307f0c87d
paras jain <parasjainooo123@gmail.com>
2026-03-09 10:19:21 +0530

    both ends are connected now

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,27 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from "axios"
 
 function App() {
+
+  const testApi = async () => {
+    const res = await axios.get("https://chatify-f1hw.onrender.com/api/login")
+    alert(res.data)
+  }
+
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <h1>Chatify Test</h1>
+
+      <button onClick={testApi}>
+        Test Backend
+      </button>
+
+      <br /><br />
+
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -16,18 +31,15 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+
       <h1>Vite + React</h1>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
